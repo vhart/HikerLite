f2e1d6d62e951f9adac327ebb93f6d4b2c026dcb
elberdev <elberdev@gmail.com>
2015-10-18 12:29:09 -0400

    added icons and new view controller for text editing

---
+++ HikerLite/TextEntryViewController.m
@@ -0,0 +1,37 @@
+//
+//  TextEntryViewController.m
+//  HikerLite
+//
+//  Created by Elber Carneiro on 10/18/15.
+//  Copyright © 2015 Varindra Hart. All rights reserved.
+//
+
+#import "TextEntryViewController.h"
+
+@interface TextEntryViewController ()
+
+@end
+
+@implementation TextEntryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
